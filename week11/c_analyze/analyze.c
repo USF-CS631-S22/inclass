985d97437072687b64ff4289970d7b1399142000
Greg Benson <benson@cs.usfca.edu>
2022-04-07 17:13:12 -0700

    Added B, BCC, BL counts.

--- week11/c_analyze/analyze.c
+++ week11/c_analyze/analyze.c
@@ -19,7 +19,9 @@ struct analyze_st {
 	int inst_count;
 	int dp_count;
 	int mem_count;
-	int branch_count;
+	int b_count;
+    int bcc_count;
+    int bl_count;
 	int mul_count;
 	int bx_count;
 };
@@ -28,7 +30,9 @@ void analyze_init(struct analyze_st *ap) {
 	ap->inst_count = 0;
 	ap->dp_count = 0;
 	ap->mem_count = 0;
-	ap->branch_count = 0;
+	ap->b_count = 0;
+	ap->bcc_count = 0;
+	ap->bl_count = 0;
 	ap->mul_count = 0;
 	ap->bx_count = 0;
 }
@@ -36,9 +40,11 @@ void analyze_init(struct analyze_st *ap) {
 void analyze_print(struct analyze_st *ap, char *name) {
 	printf("[Instruction Analysis: %s]\n", name);
 	printf("Data Processing    : %d\n", ap->dp_count);
-	printf("Memory             : %d\n", ap->mem_count);
-	printf("Branch             : %d\n", ap->branch_count);
 	printf("Multiply           : %d\n", ap->mul_count);
+	printf("Memory             : %d\n", ap->mem_count);
+	printf("B                  : %d\n", ap->b_count);
+	printf("BCC                : %d\n", ap->bcc_count);
+	printf("BL                 : %d\n", ap->bl_count);
 	printf("BX                 : %d\n", ap->bx_count);
 	printf("--------------------\n");
 	printf("Total              : %d\n", ap->inst_count);	
@@ -47,8 +53,10 @@ void analyze_print(struct analyze_st *ap, char *name) {
 void analyze_code(struct analyze_st *ap, uint32_t *code) {
 	uint32_t *ip = code;
 	uint32_t iw;
+	uint32_t bits31_28;
 	uint32_t bits27_26;
 	uint32_t bits27_25;
+	uint32_t bit24;
 	uint32_t bits27_22;
 	uint32_t bits7_4;
 	uint32_t bits27_4;
@@ -61,9 +69,11 @@ void analyze_code(struct analyze_st *ap, uint32_t *code) {
 			break;
 		}
 
+        bits31_28 = (iw >> 28) & 0b1111;
 		bits27_26 = (iw >> 26) & 0b11;
 		bits27_25 = (iw >> 25) & 0b111;
 		bits27_22 = (iw >> 22) & 0b111111;
+		bit24     = (iw >> 24) & 0b1;
 		bits7_4   = (iw >> 4) & 0b1111;
 		bits27_4  = (iw >> 4) & 0xFFFFFF;
 		
@@ -71,8 +81,15 @@ void analyze_code(struct analyze_st *ap, uint32_t *code) {
 			ap->bx_count += 1;
 		} else if (bits27_22 == 0b000000 && bits7_4 == 0b1001) {
 			ap->mul_count += 1;
-		} else if (bits27_25 == 0b101) {
-			ap->branch_count += 1;
+		} else if (bits27_25 == 0b101 && bit24 == 0 
+		           && bits31_28 == 0b1110) {
+			ap->b_count += 1;
+		} else if (bits27_25 == 0b101 && bit24 == 0
+		           && bits31_28 != 0b1110) {
+			ap->bcc_count += 1;
+		} else if (bits27_25 == 0b101 && bit24 == 1
+		           && bits31_28 == 0b1110) {
+			ap->bl_count += 1;
 		} else if (bits27_26 == 0b01) {
 			ap->mem_count += 1;
 		} else if (bits27_26 == 0b00) {
